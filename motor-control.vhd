5c4a348031719fa1b22f49fe4fda8611720be92b
Naomi <naomiguijt@hotmail.com>
2023-03-14 17:17:31 +0100

    Motor control working (no counter yet) but FSm is perFecto

--- motor-control.vhd
+++ motor-control.vhd
@@ -15,69 +15,60 @@ end entity motor_controller;
 
 architecture behavioural of motor_controller is
     type motor_controller_state is (motor_off,
-                                    motor_ccw,
-                                    motor_cw);
+                                    motor_moving,
+                                    motor_passive);
     signal state, new_state: motor_controller_state;
-    signal internal_clk : std_logic;
-
+    signal pwm_internal : std_logic;
 begin
-    process(countin)
+    process(clk)
     begin
-        if(to_integer(unsigned(countin)) < 500000) then
-            internal_clk <= '1';
-        elsif(to_integer(unsigned(countin)) < 1000000) then
-            internal_clk <= '0';
-        else
-            timebase_rst <= '1';
-        end if;
-    end process;
-
-    process(internal_clk)
-    begin
-        if(rising_edge(internal_clk)) then
-		if(reset = '1') then
+        if(rising_edge(clk)) then
+		 if(reset = '1') then
                 	state <= motor_off;
             	else
                 	state <= new_state;
             	end if;
-		
-		timebase_rst <= '1';
-	else	
-		timebase_rst <= '0';
-	end if;
+	    end if;
     end process;
 
-    process(state)
+    process(state, direction)
     begin
+	    report "The current state is " &motor_controller_state'image(state);
         case state is
             when motor_off =>
-                pwm <= '0';
-
-                if(direction = '1') then
-                    new_state <= motor_cw;
-                else
-                    new_state <= motor_ccw;
+                pwm_internal <= '0';
+                
+                if (direction = '0') then --ccw
+                    new_state <= motor_moving;
+                elsif (direction = '1') then --cw
+                    new_state <= motor_moving;
+		        else
+		            new_state <= motor_off;
                 end if;
 
-            when motor_ccw =>
-                pwm <= '1';
-		pwm <= '0' after 1 ns;
-
-                if(direction = '1') then
-                    new_state <= motor_cw;
-                else
-                    new_state <= motor_ccw;
+            when motor_moving => --move motor cw or ccw
+                pwm_internal <= '1';
+                
+                if (direction = '0') then --ccw
+                    new_state <= motor_passive after 1 ms;
+                elsif (direction = '1') then --cw
+                    new_state <= motor_passive after 2 ms;
+		        else
+		            new_state <= motor_off;
                 end if;
             
-            when motor_cw =>
-                pwm <= '1';
-		pwm <= '0' after 2 ns;
+            when motor_passive =>
+                pwm_internal <= '0';
 
-                if(direction = '1') then
-                    new_state <= motor_cw;
-                else
-                    new_state <= motor_ccw;
+                if (direction = '0') then --ccw
+                    new_state <= motor_moving after 19 ms;
+                elsif (direction = '1') then --cw
+                    new_state <= motor_moving after 18 ms;
+		else
+		    new_state <= motor_off;
                 end if;
         end case;
     end process;
+
+	pwm <= std_logic(pwm_internal); --pwm output is the internal pwm (working)
 end architecture behavioural;
